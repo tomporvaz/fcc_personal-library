70e41b5ce1155df8b56f4341d28f96e23ef74ce8
tomporvaz <tomporvaz@gmail.com>
2019-10-13 20:09:15 -0400

    Added delete all route.

--- routes/api.js
+++ routes/api.js
@@ -71,6 +71,9 @@ module.exports = function (app) {
   
   .delete(function(req, res){
     //if successful response will be 'complete delete successful'
+    Book.deleteMany()
+    .then(res.send('complete delete successful'))
+    .catch(err => console.error(err))
   });
   
   
